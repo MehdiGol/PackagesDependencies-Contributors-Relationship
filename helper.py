e8295ceebff9b8e075f46bba7a9d55bf1b0f5c09
MehdiGol <golzadeh.mehdi@gmail.com>
2018-10-18 12:22:29 +0330

    some fixes to commits and new files to retrive commit comments

--- helper.py
+++ helper.py
@@ -6,6 +6,30 @@ CURRENT_PATH = pathlib.Path(__file__).absolute().parent
 
 DATA_PATH = CURRENT_PATH / 'data'
 
+GIT_API = ['5da1276e73fbfda3c268:35e6eb9e99f74000131e20e4caa644a546dd384d',
+           'e96241ca553fb9cf4d1b:979ecb8c98fdcddfa7921623daa4dacb3fa415b4',
+           '7d401956aa5c0afcf7f9:0f89c5e4ecd30a36aef65f3b0dcbbd120d534fb0',
+           'fe0f71d5267a85bb0345:05d5125ab433595d8e15d3cbb1ad13ae95c1d9c2',
+           '76997fa7dd9cf9f4872f:d8754621a63f1d33d5c0427b2efafb94141b8c70',
+           '53a9c2f81aeb70d453b9:8bb7576c10cf1340e833acc791a7259786ca11c8',
+           '093a7ed958a206f60a37:3dc925411cdea302056f7d22a5123efe2dbad079',
+           '9fd9b488461473c436a6:3545408a65ba7929f28a56fbc2475bd3c592379b',
+           'a490a4a97ea7b9cab562:a8412ae2115935e1a6313c535f666ff9ecca4c79',
+           '57aabf73d33083f3c49f:cf81d09889b6c768eab39e2c8c259bfc8b1c4488',
+           '165af6758225e8017e62:cb905bd2ab24e39b10f47a99eec012c65b4a566f',
+           '0b2f27dac3c25848eb7f:4f9d27ca9810ce46f7a317c63dde389f287aa4dc',
+	   'ea4b6eec52dce28ec7fa:7959c04f36e84e8c701279b3cea1aef8b596088a',
+           '13ebff80c1f67316e889:2e9cc995164db8438dd7f776ab53af4592353c1f',
+           'be243f299d5b56b105e0:3982847d4312aa7cbed09da43cf6f39ace1beb6a',
+           '1b08271f98996e347123:d8e0dfbe1fd136b76585a50d7e89b80492f0fb4e',
+           'fe57e2e032d3230c1766:244315313d02e9146061bc6946c61d145121fbbe',
+           'c2bf131a500f55730b6c:b9e8de22e6575495eb493b18fcebd48d0df6ffef',
+           '2d7a20e9b74893c3aca7:ceeb0fb792c336f8567981881798d906f9948118',
+           'dda03c75918b37514d85:8e1760a5500fd75d73452e0a8329d0b1f15c4be2',
+           '1ba77e5c592d462e7ae4:535199ceacbda6130156b0ebda64926a6e139c5d',
+           'ff9e74acc730af8ead4d:811f5bee820d058aad60d32fed3074d535201f95',
+           'ef066c3b2f51f82e904b:11984fde063725cb1c0e982033903d52b789b562',
+           'bbb3a7cd0b231237aac5:c9e66b2b92ee97660e2c38254f802e305b8e8ab4']
 
 def clean_data(packages, dependencies, ecosystem=None):
     """
